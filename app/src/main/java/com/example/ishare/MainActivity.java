3994b77961674604733edc08aa79f908cf312170
IHATEYOU1998 <solomons@mit.edu>
2019-10-13 01:21:46 -0400

    Basic App

--- app/src/main/java/com/example/ishare/MainActivity.java
+++ app/src/main/java/com/example/ishare/MainActivity.java
@@ -21,51 +21,75 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 
+import android.widget.Button;
+import android.content.Intent;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        login_button = (Button) findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                openActivity2();
             }
         });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
     }
+    public void openActivity2() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+
+
+
+
+/**
+ *
+ * Toolbar toolbar = findViewById(R.id.toolbar);
+ *         setSupportActionBar(toolbar);
+ *         FloatingActionButton fab = findViewById(R.id.fab);
+ *         fab.setOnClickListener(new View.OnClickListener() {
+ *             @Override
+ *             public void onClick(View view) {
+ *                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+ *                         .setAction("Action", null).show();
+ *             }
+ *         });
+ *         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+ *         NavigationView navigationView = findViewById(R.id.nav_view);
+ *         // Passing each menu ID as a set of Ids because each
+ *         // menu should be considered as top level destinations.
+ *         mAppBarConfiguration = new AppBarConfiguration.Builder(
+ *                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+ *                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+ *                 .setDrawerLayout(drawer)
+ *                 .build();
+ *         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+ *         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+ *         NavigationUI.setupWithNavController(navigationView, navController);
+ *     }
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
+        }
 
-    @Override
-    public boolean onSupportNavigateUp() {
+@Override
+public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-
-
-    }
-}
+        || super.onSupportNavigateUp();
+        }
+        **/
