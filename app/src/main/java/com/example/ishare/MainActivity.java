f0b67041469b6853179bdc9d337ddbd37cf32605
IHATEYOU1998 <solomons@mit.edu>
2019-10-13 22:05:27 -0400

    Basic App

--- app/src/main/java/com/example/ishare/MainActivity.java
+++ app/src/main/java/com/example/ishare/MainActivity.java
@@ -1,6 +1,73 @@
 package com.example.ishare;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText firstName;
+    EditText lastName;
+    EditText email;
+    Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkDataEntered();
+            }
+        });
+    }
+
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    void checkDataEntered() {
+        if (isEmpty(firstName)) {
+            Toast t = Toast.makeText(this, "You must enter first name to register!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+
+        if (isEmpty(lastName)) {
+            lastName.setError("Last name is required!");
+        }
+
+        if (!isEmail(email)) {
+            email.setError("Enter valid email!");
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+/**
+package com.example.ishare;
+
+import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -27,69 +94,49 @@ import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        login_button = (Button) findViewById(R.id.login_button);
-        login_button.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                openActivity2();
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
-    public void openActivity2() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-}
 
-
-
-
-
-
-
-/**
- *
- * Toolbar toolbar = findViewById(R.id.toolbar);
- *         setSupportActionBar(toolbar);
- *         FloatingActionButton fab = findViewById(R.id.fab);
- *         fab.setOnClickListener(new View.OnClickListener() {
- *             @Override
- *             public void onClick(View view) {
- *                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
- *                         .setAction("Action", null).show();
- *             }
- *         });
- *         DrawerLayout drawer = findViewById(R.id.drawer_layout);
- *         NavigationView navigationView = findViewById(R.id.nav_view);
- *         // Passing each menu ID as a set of Ids because each
- *         // menu should be considered as top level destinations.
- *         mAppBarConfiguration = new AppBarConfiguration.Builder(
- *                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
- *                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
- *                 .setDrawerLayout(drawer)
- *                 .build();
- *         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
- *         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
- *         NavigationUI.setupWithNavController(navigationView, navController);
- *     }
-@Override
-public boolean onCreateOptionsMenu(Menu menu) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-        }
+    }
 
-@Override
-public boolean onSupportNavigateUp() {
+    @Override
+    public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-        || super.onSupportNavigateUp();
-        }
-        **/
+                || super.onSupportNavigateUp();
+    }
+}
+
+ **/
